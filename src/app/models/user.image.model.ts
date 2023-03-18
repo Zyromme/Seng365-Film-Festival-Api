4b337c41bd54e9cc5f83f6ca2a855c0a384f99d6
Zyromme Bayson <zba29@uclive.ac.nz>
2023-03-18 22:35:00 +1300

    added insertImage and deleteImage in the user.image.model.ts

--- src/app/models/user.image.model.ts
+++ src/app/models/user.image.model.ts
@@ -9,11 +9,25 @@ const getOne = async (id: number): Promise<any> => {
     const query = `SELECT image_filename from user where id = ?`;
     const [ result ] = await conn.query(query, [ id ]);
     await conn.release();
+    await conn.release();
     return result;
 }
 
-// const setImage = async (image: string): Promise<ResultSetHeader> => {
- //   logger.info(`Setting users image to ${image}`)
-// }
+const setImage = async (id: number, image: string): Promise<ResultSetHeader> => {
+    logger.info(`Setting user ${id}'s image to ${image}`);
+    const conn = await getPool().getConnection();
+    const query = `UPDATE user set image_filename = ? where id = ?`;
+    const [ result ] = await conn.query(query, [image, id]);
+    return result;
+}
 
-export {getOne}
+const deleteImage = async (id:number): Promise<ResultSetHeader> => {
+    logger.info(`Deleting user ${id}'s image`);
+    const conn = await getPool().getConnection();
+    const query = `UPDATE user set image_filename = NULL where id = ?`;
+    const [ result ] = await conn.query(query, [ id ])
+    await conn.release();
+    return result;
+}
+
+export {getOne, setImage, deleteImage}
